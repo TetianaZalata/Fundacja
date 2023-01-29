ec7dd5cedaa27e3eaa6321cf1ba463504431507e
dev oz <ownlocalstorage@gmail.com>
2023-01-29 21:36:43 +0200

    fix

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,15 +3,15 @@
         <section class="text-justify subtitle-2">
             <h3>Fundacja Wspierania<br> i Rozwoju Edukacji</h3>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-phone</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiPhone" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href="tel:608 096 437">608 096 437</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-at</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiAt" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href = "mailto:kontakt@fedukacja.org.pl">kontakt@fedukacja.org.pl</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-map-marker</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiMapMarker" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a :href="mapLinkFund" target="_blank">ul. Gębarzewska 12<br><div class="space"/> 62-200 Gniezno</a>
             </div>
         </section>
@@ -19,15 +19,15 @@
         <section class="text-justify subtitle-2">
             <h3>Szkoła Podstawowa<br> i Liceum Ogólnokształcące PORKOM</h3>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-phone</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiPhone" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href="tel:664 188 793">664 188 793</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-at</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiAt" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href = "mailto:sekretariat@porkom.com.pl">sekretariat@porkom.com.pl</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-map-marker</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiMapMarker" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a :href="mapLinkFund" target="_blank">ul. Gębarzewska 12<br><div class="space"/> 62-200 Gniezno</a>
             </div>
         </section>
@@ -35,15 +35,15 @@
         <section class="text-justify subtitle-2">
             <h3>Przedszkole<br> Leśna Kraina</h3>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-phone</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiPhone" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href="tel:664 188 793">664 188 793</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-at</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiAt" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href = "mailto:sekretariat@porkom.com.pl">sekretariat@porkom.com.pl</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-map-marker</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiMapMarker" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a :href="mapLinkFund" target="_blank">ul. Gębarzewska 12<br><div class="space"/> 62-200 Gniezno</a>
             </div>
         </section>
@@ -51,15 +51,15 @@
         <section class="text-justify subtitle-2">
             <h3>Żłobek<br> Zaczarowany Zakątek</h3>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-phone</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiPhone" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href="tel:698 947 113">698 947 113</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-at</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiAt" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a href = "mailto:kontakt@zakatekzlobek.pl">kontakt@zakatekzlobek.pl</a>
             </div>
             <div class="caption">
-                <v-icon class="mr-5" small>mdi-map-marker</v-icon>
+                <svg-icon class="mr-5" type="mdi" :path="mdiMapMarker" :style="{width: '14px', height: '14px'}"></svg-icon>
                 <a :href="mapLinkZlobek" target="_blank">os. Władysława Łokietka 15<br><div class="space"/> 62-200 Gniezno</a>
             </div>
         </section>
@@ -68,12 +68,20 @@
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiPhone, mdiAt, mdiMapMarker } from "@mdi/js";
 export default {
     name: "TheFooter",
+    components: {
+        SvgIcon,
+    },
     data() {
         return {
             mapLinkFund: 'https://www.google.com/maps/place/G%C4%99barzewska+12,+62-200+Gniezno,+Poland/@52.5018365,17.5867376,18.75z/data=!4m5!3m4!1s0x470493dbf97748df:0xb818811e6a2e31a9!8m2!3d52.5018513!4d17.5871274',
             mapLinkZlobek: 'https://www.google.com/maps/place/Osiedle+W%C5%82adys%C5%82awa+%C5%81okietka+15,+62-200+Gniezno,+Poland/@52.5467833,17.5977159,17z/data=!3m1!4b1!4m5!3m4!1s0x470491017bf65c9b:0x2535b45bbedff67b!8m2!3d52.5467801!4d17.5999046',
+            mdiPhone,
+            mdiAt,
+            mdiMapMarker,
         };
     },
 };
